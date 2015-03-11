a01ad13ea414e7c9dd96fdd33486afd9ed9f7a58
npkhanh <franknguyen007@gmail.com>
2015-03-11 10:30:40 +0700

    fix bug

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ canvasWidth = 820
 canvasHeight = 820
 sys.setrecursionlimit(10000)
 
-gui = graphic.GUI(820, 820, mazeSize)
+gui = graphic.GUI(canvasWidth, canvasHeight, mazeSize)
 gui.createWindow()
 gui.run()
 
