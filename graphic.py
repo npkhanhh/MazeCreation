1f00de09f0fae4f1028d301965d2160444fb2da5
dhuy93 <d.huy93@gmail.com>
2015-07-15 11:33:31 +0700

    Replace the RegionSolver by RegionSolver_FW, modified graphic.py to use RegionSolver_FW *RegionSolver_FW find paths and deadends, then stores them in 2 2D-arrays.

--- graphic.py
+++ graphic.py
@@ -1,4 +1,6 @@
 from FindSolution import FindSolution
+from RegionSolver_FW import RegionSolver_FW
+from FloydWarshallImpl import FloydWarshallImpl
 __author__ = 'Khanh'
 try:
     import Tkinter as tk
@@ -149,13 +151,193 @@ class GUI:
         self.runButton.grid(row=17, column=1, columnspan=3)
         self.overlapButton.grid(row=18, column=1, columnspan=3)
 
+    def setupTest1(self):
+        self.grid[0][0].top = 1
+        self.grid[0][0].bottom = 0
+        self.grid[0][0].left = 1
+        self.grid[0][0].right = 0
+        
+        self.grid[0][1].top = 1
+        self.grid[0][1].bottom = 1
+        self.grid[0][1].left = 0
+        self.grid[0][1].right = 0
+        
+        self.grid[0][2].top = 1
+        self.grid[0][2].bottom = 1
+        self.grid[0][2].left = 0
+        self.grid[0][2].right = 0
+        
+        self.grid[0][3].top = 1
+        self.grid[0][3].bottom = 0
+        self.grid[0][3].left = 0
+        self.grid[0][3].right = 1
+        
+        self.grid[0][4].top = 1
+        self.grid[0][4].bottom = 1
+        self.grid[0][4].left = 1
+        self.grid[0][4].right = 0
+        
+        self.grid[0][5].top = 1
+        self.grid[0][5].bottom = 0
+        self.grid[0][5].left = 0
+        self.grid[0][5].right = 1
+        
+        self.grid[1][0].top = 0
+        self.grid[1][0].bottom = 0
+        self.grid[1][0].left = 1
+        self.grid[1][0].right = 0
+        
+        self.grid[1][1].top = 1
+        self.grid[1][1].bottom = 1
+        self.grid[1][1].left = 0
+        self.grid[1][1].right = 0
+        
+        self.grid[1][2].top = 1
+        self.grid[1][2].bottom = 0
+        self.grid[1][2].left = 0
+        self.grid[1][2].right = 1
+        
+        self.grid[1][3].top = 0
+        self.grid[1][3].bottom = 1
+        self.grid[1][3].left = 1
+        self.grid[1][3].right = 0
+        
+        self.grid[1][4].top = 1
+        self.grid[1][4].bottom = 1
+        self.grid[1][4].left = 0
+        self.grid[1][4].right = 0
+        
+        self.grid[1][5].top = 0
+        self.grid[1][5].bottom = 1
+        self.grid[1][5].left = 0
+        self.grid[1][5].right = 1
+        
+        self.grid[2][0].top = 0
+        self.grid[2][0].bottom = 1
+        self.grid[2][0].left = 1
+        self.grid[2][0].right = 0
+        
+        self.grid[2][1].top = 1
+        self.grid[2][1].bottom = 0
+        self.grid[2][1].left = 0
+        self.grid[2][1].right = 1
+        
+        self.grid[2][2].top = 10
+        self.grid[2][2].bottom = 1
+        self.grid[2][2].left = 1
+        self.grid[2][2].right = 0
+        
+        self.grid[2][3].top = 1
+        self.grid[2][3].bottom = 1
+        self.grid[2][3].left = 0
+        self.grid[2][3].right = 0
+        
+        self.grid[2][4].top = 1
+        self.grid[2][4].bottom = 1
+        self.grid[2][4].left = 0
+        self.grid[2][4].right = 0
+        
+        self.grid[2][5].top = 1
+        self.grid[2][5].bottom = 0
+        self.grid[2][5].left = 0
+        self.grid[2][5].right = 1
+        
+        self.grid[3][0].top = 1
+        self.grid[3][0].bottom = 0
+        self.grid[3][0].left = 1
+        self.grid[3][0].right = 0
+        
+        self.grid[3][1].top = 0
+        self.grid[3][1].bottom = 1
+        self.grid[3][1].left = 0
+        self.grid[3][1].right = 1
+        
+        self.grid[3][2].top = 1
+        self.grid[3][2].bottom = 1
+        self.grid[3][2].left = 1
+        self.grid[3][2].right = 0
+        
+        self.grid[3][3].top = 1
+        self.grid[3][3].bottom = 0
+        self.grid[3][3].left = 0
+        self.grid[3][3].right = 0
+        
+        self.grid[3][4].top = 1
+        self.grid[3][4].bottom = 1
+        self.grid[3][4].left = 0
+        self.grid[3][4].right = 1
+        
+        self.grid[3][5].top = 0
+        self.grid[3][5].bottom = 0
+        self.grid[3][5].left = 1
+        self.grid[3][5].right = 1
+        
+        self.grid[4][0].top = 0
+        self.grid[4][0].bottom = 0
+        self.grid[4][0].left = 1
+        self.grid[4][0].right = 1
+        
+        self.grid[4][1].top = 1
+        self.grid[4][1].bottom = 0
+        self.grid[4][1].left = 1
+        self.grid[4][1].right = 0
+        
+        self.grid[4][2].top = 1
+        self.grid[4][2].bottom = 1
+        self.grid[4][2].left = 0
+        self.grid[4][2].right = 0
+        
+        self.grid[4][3].top = 0
+        self.grid[4][3].bottom = 0
+        self.grid[4][3].left = 0
+        self.grid[4][3].right = 1
+        
+        self.grid[4][4].top = 1
+        self.grid[4][4].bottom = 1
+        self.grid[4][4].left = 1
+        self.grid[4][4].right = 0
+        
+        self.grid[4][5].top = 0
+        self.grid[4][5].bottom = 1
+        self.grid[4][5].left = 0
+        self.grid[4][5].right = 1
+        
+        self.grid[5][0].top = 0
+        self.grid[5][0].bottom = 1
+        self.grid[5][0].left = 1
+        self.grid[5][0].right = 0
+        
+        self.grid[5][1].top = 0
+        self.grid[5][1].bottom = 1
+        self.grid[5][1].left = 0
+        self.grid[5][1].right = 1
+        
+        self.grid[5][2].top = 1
+        self.grid[5][2].bottom = 1
+        self.grid[5][2].left = 1
+        self.grid[5][2].right = 0
+        
+        self.grid[5][3].top = 0
+        self.grid[5][3].bottom = 1
+        self.grid[5][3].left = 0
+        self.grid[5][3].right = 0
+        
+        self.grid[5][4].top = 1
+        self.grid[5][4].bottom = 1
+        self.grid[5][4].left = 0
+        self.grid[5][4].right = 0
+        
+        self.grid[5][5].top = 1
+        self.grid[5][5].bottom = 1
+        self.grid[5][5].left = 0
+        self.grid[5][5].right = 1
     def doHuysStuff(self):
         # TODO: use bots to explore maze 
         ########## Explore ##########
         nRegion = 2
         regionSize = self.size / nRegion
         self.regionMap = [[[] for x in range(nRegion)] for y in range(nRegion)]
-        lock = threading.Lock()
+        """lock = threading.Lock()
         threads = []
         for i in range(nRegion):
             for j in range(nRegion):
@@ -166,7 +348,29 @@ class GUI:
         for t in threads:
             t.join()
 
+        print(self.regionMap)"""
+        #############################################################################################
+        #self.setupTest1()
+        deMap = [[[] for x in range(nRegion)] for y in range(nRegion)]
+        lock = threading.Lock()
+        threads = []
+        for i in range(nRegion):
+            for j in range(nRegion):
+                regSolver = RegionSolver_FW(self.grid, [i*regionSize, (i+1)*regionSize, j*regionSize, (j+1)*regionSize], i, j, self.regionMap, deMap, lock)
+                threads.append(regSolver)
+                regSolver.start()
+        
+        for t in threads:
+            t.join()
+
+        print "Path Map\n"
         print(self.regionMap)
+        print "\nDeadend Map\n"
+        print(deMap)
+        
+        #fw = FloydWarshallImpl(self.regionMap, deMap, nRegion)
+        
+        #############################################################################################
 
 
 
